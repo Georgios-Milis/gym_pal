4145d8585f828c02d1dd796ad27704e99cb6721b
Georgios Milis <el18035@mail.ntua.gr>
2022-03-01 19:11:40 +0200

    Basic buggy Home

--- lib/views/home/home_logged_in.dart
+++ lib/views/home/home_logged_in.dart
@@ -10,12 +10,33 @@ class _HomeLoggedInState extends State<HomeLoggedIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Home'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Home'),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+        ),
+        body: Center(
+          child: <Widget>[
+            Text("Hi! I'm Pandy your Gym Pal!"),
+            Image.asset('assets/Panda.png'),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/cardio-purple.png'),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/test-results-grey.png'),
+              ),
+            ],
+          ),
+          color: Colors.blue,
+        ));
   }
 }
