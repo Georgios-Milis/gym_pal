8356fce20696338a81d76dec9cfa482a5c37d454
Georgios Milis <el18035@mail.ntua.gr>
2022-03-01 19:52:41 +0200

    Basic Home

--- lib/views/home/home_logged_in.dart
+++ lib/views/home/home_logged_in.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomeLoggedIn extends StatefulWidget {
@@ -7,38 +9,57 @@ class HomeLoggedIn extends StatefulWidget {
 }
 
 class _HomeLoggedInState extends State<HomeLoggedIn> {
+  String username = "George";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {},
-          ),
+      appBar: AppBar(
+        title: Text('Welcome, $username!'),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              const Text("Hi! I'm Pandy your Gym Pal!"),
-              Image.asset('../../../assets/Panda.png'),
-            ],
-          ),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text("Hi! I'm Pandy your Gym Pal!"),
+            const Text(
+                "Let's create some custom sessions so that we can work out together!"),
+            SizedBox(
+              width: 200,
+              child: Image.asset('../../../assets/Panda.png'),
+            ),
+            SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("START NOW!"),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('../../../assets/cardio-purple.png'),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('../../../assets/test-results-grey.png'),
+                ),
+              ],
+            ),
+          ],
         ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: <Widget>[
-              IconButton(
-                onPressed: () {},
-                icon: Image.asset('../../../assets/cardio-purple.png'),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Image.asset('../../../assets/test-results-grey.png'),
-              ),
-            ],
-          ),
-          color: Colors.blue,
-        ));
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: const <Widget>[],
+        ),
+        color: Colors.blue,
+      ),
+    );
   }
 }
