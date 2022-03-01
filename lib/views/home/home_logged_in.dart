99772f989339069ad466a1d09c0c5a653b16346b
thomas <thomas@staging.gr>
2022-03-02 00:14:49 +0200

    Add sample external widget for appBar

--- lib/views/home/home_logged_in.dart
+++ lib/views/home/home_logged_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:gym_pal/widgets/header.dart';
+
 class HomeLoggedIn extends StatefulWidget {
   const HomeLoggedIn({Key? key}) : super(key: key);
   @override
@@ -10,11 +12,12 @@ class _HomeLoggedInState extends State<HomeLoggedIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
+      appBar: header(context,
+          isAppTitle: false,
+          titleText: 'You are logged in',
+          removeBackButton: true),
       body: const Center(
-        child: Text('Home'),
+        child: Text('This is the home page text!'),
       ),
     );
   }
