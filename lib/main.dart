cf2aa6b73dced65ce7dcb898cb6812ad4de3c82f
thomas <thomas@staging.gr>
2022-03-03 15:43:39 +0200

    Fix build.gradle. Add packages needed for authenticatino. Add images and fonts. Add basic authentication in home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:gym_pal/views/home/home_logged_in.dart';
+
+import 'package:gym_pal/views/home/home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Gym Pal',
     theme: ThemeData(
-      primarySwatch: Colors.blue,
+      primarySwatch: Colors.purple,
+      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.teal),
     ),
-    home: const HomePage(),
+    home: const Home(),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeLoggedIn(),
-    );
-  }
 }
---
+++ lib/views/home/home.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      if (account != null) {
+        print('User signed in: $account');
+        setState(() {
+          isAuth = true;
+        });
+        // handleSignIn(account);
+      } else {
+        setState(() {
+          isAuth = false;
+        });
+      }
+    }, onError: (err) {
+      print('Error: $err');
+    });
+    googleSignIn.signInSilently();
+  }
+
+  login() {
+    print('clicked');
+    // googleSignIn.signIn();
+  }
+
+  Widget buildAuthScreen() {
+    return const Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.teal,
+              Colors.purple,
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Gym Pal',
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            const Image(
+                image: AssetImage('assets/images/welcome_to_gym_buddy3.png')),
+            GestureDetector(
+              onTap: () => login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
