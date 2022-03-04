6970b5b94c668b4cb9b287099035d308478f17bf
Georgios Milis <el18035@mail.ntua.gr>
2022-03-04 02:38:27 +0200

    Complete scaffold for Repetitive workout

--- lib/views/workouts/addWorkout.dart
+++ lib/views/workouts/addWorkout.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:backdrop/backdrop.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:gym_pal/widgets/header.dart';
 import 'package:gym_pal/widgets/sidenav.dart';
 import 'package:gym_pal/widgets/bottom.dart';
+
+class CounterWidget extends StatefulWidget {
+  final String title;
+
+  const CounterWidget({Key? key, this.title = "Count"}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _EditCounterWidgetState();
+}
+
+class _EditCounterWidgetState extends State<CounterWidget> {
+  int _count = 0;
+
+  void _incrementCount() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _decrementCount() {
+    setState(() {
+      if (_count > 0) {
+        _count--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(widget.title),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: Text("$_count"),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: IconButton(
+              onPressed: _incrementCount,
+              icon: const Icon(Icons.arrow_drop_up),
+              tooltip: "More",
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: IconButton(
+              onPressed: _decrementCount,
+              icon: const Icon(Icons.arrow_drop_down),
+              tooltip: "Less",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class ViewEditWorkoutWidget extends StatefulWidget {
   const ViewEditWorkoutWidget({Key? key}) : super(key: key);
@@ -14,6 +72,9 @@ class ViewEditWorkoutWidget extends StatefulWidget {
 }
 
 class _ViewEditWorkoutWidgetState extends State<ViewEditWorkoutWidget> {
+  final _formKey = GlobalKey<FormState>();
+  bool _audio = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,84 +83,92 @@ class _ViewEditWorkoutWidgetState extends State<ViewEditWorkoutWidget> {
         child: sidenav(context),
       ),
       bottomNavigationBar: bottom(),
-      body: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent[700],
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.close),
-            color: Colors.white,
-          ),
-          actions: <Widget>[
-            IconButton(
+      body: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurpleAccent[700],
+            leading: IconButton(
               onPressed: () {
                 Navigator.pop(context);
               },
-              icon: const Icon(Icons.check),
+              icon: const Icon(Icons.close),
               color: Colors.white,
             ),
-          ],
-        ),
-        body: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Title",
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                    ),
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("Customize your workout! Is it repetitive?"),
-              ),
-              Center(
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        const Text("Sets:"),
-                        IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.arrow_upward),
-                        ),
-                        IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.arrow_downward),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: <Widget>[
-                        const Text("Reps:"),
-                        IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.arrow_upward),
-                        ),
-                        IconButton(
-                          onPressed: null,
-                          icon: Icon(Icons.arrow_downward),
-                        ),
-                      ],
-                    ),
-                    const Text("Hey pal! Want audio feedback while training?"),
-                    SizedBox(
-                      width: 200,
-                      child:
-                          Image.asset('../../../assets/panda-victorious.png'),
-                    ),
-                  ],
-                ),
+            actions: <Widget>[
+              IconButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pop(context);
+                  }
+                },
+                icon: const Icon(Icons.check),
+                color: Colors.white,
               ),
             ],
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "REPETITIVE"),
+                Tab(text: "TIMED"),
+              ],
+            ),
+          ),
+          body: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Title",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Title cannot be empty!";
+                        }
+                        return null;
+                      }),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Customize your workout! Is it repetitive?"),
+                ),
+                Center(
+                  child: Column(
+                    children: <Widget>[
+                      const CounterWidget(title: "Sets: "),
+                      const CounterWidget(title: "Reps: "),
+                      const Text(
+                          "Hey pal! Want audio feedback while training?"),
+                      SizedBox(
+                        width: 200,
+                        child: Image.asset('assets/panda-victorious.png'),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("No, thanks"),
+                          Switch(
+                            value: _audio,
+                            onChanged: (value) {
+                              setState(() {
+                                _audio = value;
+                              });
+                            },
+                          ),
+                          const Text("Yes!"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
