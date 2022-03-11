fd6ccb03d9523bcd98bf0e8ee6f58c2dd7cb50ed
Thomas Minitsios <el03241@mail.ntua.gr>
2022-03-11 16:18:00 +0200

    Add getAll, deleteAll db crud operations.

--- lib/database/crud.dart
+++ lib/database/crud.dart
@@ -23,4 +23,17 @@ class DBLogic {
     final finder = Finder(filter: Filter.byKey(workout.id));
     await _workouts.delete(await db, finder: finder);
   }
+
+  Future<List<Workout>> getAllWorkouts() async {
+    final recordSnapshots = await _workouts.find(await db);
+    return recordSnapshots.map((snapshot) {
+      final workout = Workout.fromMap(snapshot.value);
+      workout.id = snapshot.key;
+      return workout;
+    }).toList();
+  }
+
+  Future deleteAll() async {
+    await _workouts.delete(await db);
+  }
 }
